5c21db0a445c16dfca422f6dceadbfcb0e99ca74
maccollege <30079306@lcwc.ac.uk>
2024-09-24 16:31:05 +0000

    Finished Work

--- task.py
+++ task.py
@@ -1,11 +1,28 @@
+import time
+
 def task_1():
     password = "Password123"
-
-    # Enter your code here
+    guess = input("Guess the password.")
+    while guess != password:
+        guess = input("Password incorrect, try again!")
+    else:
+        print("Password cracked!")
 
 def task_2(): # Times table
-
+    tt = int(input("Select your number times table."))
+    terms = int(input("How many terms should be outputted?"))
+    repeat = 1
+    while repeat <= terms:
+        output = tt*repeat
+        print(output)
+        repeat = repeat + 1
     # Enter your code here
 
-def task_3(): # Count mississippis
-    
+def task_3():
+     # Count mississippis
+     count = 1
+     while count != 6:
+         print(count, "Mississippis")
+         count = count + 1
+         time.sleep(1)
+print(task_3())
